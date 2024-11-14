626faa18fa2ac03beb7a4818d54d89b30514470e
agusesquivel <110064785+agusesquivel@users.noreply.github.com>
2024-11-13 23:00:04 -0300

    testing

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const app = express();
+
+// Define el controlador para el endpoint GET
+app.get('/saludo', (req, res) => {
+  res.send('Hola Mundo');
+});
+
+// Configura el servidor para escuchar en el puerto 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
